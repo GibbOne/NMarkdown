28551e9adf28e0c2e5059511f16d4f21c139b6e0
Gilberto Polzoni <g.polzoni@loccioni.com>
2019-09-09 16:06:36 +0800

    Add test for previous commit.
    Test the absence of null reference exception

---
+++ NMarkdownTests/TextTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NMarkdown;
+using NUnit.Framework;
+
+namespace NMarkdownTests
+{
+    public class TextTests
+    {
+        [Test]
+        public void NullTextWidthTest()
+        {
+            // ARRANGE
+            var text = new Text();
+            text.RawText = null;
+
+            // ACT
+            text.RawTextWidth.Should().Be(0);
+        }
+    }
+}
